03478b94430b80f796184b3b92cd50908a69dc29
mfcastro <mfcastro50@gmail.com>
2016-05-25 18:13:06 -0500

    Created models and databases. Created Foreign Key, Primary key

--- OneCityProject/OneCityProject/Models/IdentityModels.cs
+++ OneCityProject/OneCityProject/Models/IdentityModels.cs
@@ -9,6 +9,7 @@ namespace OneCityProject.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
@@ -23,13 +24,11 @@ namespace OneCityProject.Models
 
         public DbSet<Vote> Vote { get; set; }
 
-        public DbSet<PostLocation> PostLocation { get; set; }
+		public DbSet<PostLocation> PostLocation { get; set; }
 
-        public DbSet<Post> Post { get; set; }
+		public DbSet<Post> Post { get; set; }
 
-        public DbSet<User> User { get; set; }
-
-        public ApplicationDbContext()
+		public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
